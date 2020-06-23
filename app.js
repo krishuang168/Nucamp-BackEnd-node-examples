bc441331178dce97e9ed12aedf3406a749627ce0
Yin-Chen Kris Huang <krishuang168@Yin-Chende-MacBook.local>
2020-06-23 08:16:41 -0700

    Simple Node Module

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-const rect = {
-  perimeter: (x, y) => 2 * (x + y),
-  area: (x, y) => x * y,
-};
+const rect = require("./rectangle");
 
 function solveRect(l, w) {
   console.log(`Solving for rectangle with dimensions ${l}, ${w}`);
@@ -14,7 +11,7 @@ function solveRect(l, w) {
     console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
   }
 
-  console.log('\n')
+  console.log("\n");
 }
 
 solveRect(2, 4);

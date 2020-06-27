abdbef75e1da610b72d75517d087c745f701d48c
Yin-Chen Kris Huang <krishuang168@Yin-Chende-MacBook.local>
2020-06-27 04:37:00 -0700

    Prettier Code formatter

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ function solveRect(l, w) {
 
   rect(l, w, (err, rectangle) => {
     if (err) {
-        console.log('ERROR:', err.message);
+      console.log("ERROR:", err.message);
     } else {
-        console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
-        console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+      console.log(
+        `Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`
+      );
+      console.log(
+        `Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`
+      );
     }
-});
-console.log('This statement is logged after the call to rect()');
+  });
+  console.log("This statement is logged after the call to rect()");
 
   console.log("\n");
 }
